4c70f7906de4d7df427941b589be45aff9f4d2fa
EduardEdiJerkovic <eduard.edi.jerkovic@gmail.com>
2021-03-25 14:23:14 +0100

    Add config file.

---
+++ src/config/Configuration.tsx
@@ -0,0 +1,10 @@
+const Configuration = {
+  API_HOSTNAME: process.env.REACT_APP_API_HOSTNAME || "",
+  SERVER_URL: process.env.REACT_APP_URL_SERVER || "",
+};
+
+/**
+ * Configuration file with env variables.
+ *
+ */
+export default Configuration;
