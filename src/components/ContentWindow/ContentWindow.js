2496b24ec252266331c3caaba1ab8aa18ab80b49
zmatic5 <zvonimirmatic5@gmail.com>
2021-03-26 00:48:32 +0100

    Merged

--- src/components/ContentWindow/ContentWindow.js
+++ src/components/ContentWindow/ContentWindow.js
@@ -1,11 +1,7 @@
 import React from 'react';
-<<<<<<< HEAD
 import { Route, Switch } from 'react-router-dom';
-=======
-import { BrowserRouter as Route, Switch } from 'react-router-dom';
-import Admin from '../Admin/Admin';
->>>>>>> 96e92a8c7e1f2b71f44b86f262e93aa0f3ef035e
 
+import Admin from '../Admin/Admin';
 import Flights from '../Flights';
 import Tickets from '../Tickets/Tickets.js';
 
