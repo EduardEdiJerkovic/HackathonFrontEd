e3b258f76bc240ec6b05f6ccab64aaae62be6bfe
Plenkovic <visko@arsfutura.co>
2021-03-25 17:03:21 +0100

    Set to be collapsable

--- src/components/MainSidebar.js
+++ src/components/MainSidebar.js
@@ -13,7 +13,7 @@ const mql = window.matchMedia(`(min-width: 800px)`);
 const MainSidebar = (props) => {
   const { classes } = props;
   return (
-      <Sidebar classes={classes.sidebar} isCollapsed={false}>
+      <Sidebar classes={classes.sidebar} isCollapsed={true}>
         <LogoText>FlightHelp</LogoText>
         <DropdownItem classes={classes.menuItem}
           values={[<Link to='/tickets/reservate'>Reservate</Link>,<Link to='/tickets/buy'>Buy</Link>]}>
