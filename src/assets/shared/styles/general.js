f7531da001f5f4d6538b6b495c8b96f17a2f46a7
Plenkovic <visko@arsfutura.co>
2021-03-25 18:15:25 +0100

    Add general styles

---
+++ src/assets/shared/styles/general.js
@@ -0,0 +1,23 @@
+export const white = '#ffffff';
+export const offWhite = '#fafafa';
+export const body = '#1c2127';
+
+export const grey2 = '#e9e9e9';
+export const grey1 = '#f0f1f1';
+export const grey = '#d9e0eb';
+export const ashPale = '#d9dcde';
+export const ash = '#c4c7c9';
+
+export const warning = '#e4002b';
+
+// z-indexes
+export const zLevel1 = '100';
+export const zLevel2 = '200';
+export const zLevel3 = '300';
+export const zLevel4 = '400';
+export const zLevel5 = '500';
+
+// font-weights
+export const regular = '500';
+export const semibold = '600';
+export const bold = '700';
